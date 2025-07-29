fd9d356ce2d3ce2a82094a4c4f76080c8c27c354
Kheav-Kienghok <156294487+Kheav-Kienghok@users.noreply.github.com>
2025-07-29 18:51:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/Portfolio_Website/',
+  base: './',
+  // base: '/Portfolio_Website/',
   plugins: [react(),
     tailwindcss(),
   ],
