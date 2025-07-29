923e2264eef9ad1b35ad40f2dc6e7fb5639564ac
Kheav-Kienghok <156294487+Kheav-Kienghok@users.noreply.github.com>
2025-07-29 12:16:43 +0000

    Update absolute path

--- src/pages/components/BackgroundContainer.jsx
+++ src/pages/components/BackgroundContainer.jsx
@@ -4,7 +4,7 @@ const BackgroundContainer = ({ children }) => {
   return (
     <div
       className="bg-cover bg-center min-h-screen sm:h-auto fixed w-screen z-[-1]"
-      style={{ backgroundImage: 'url(/Portfolio_Website/images/Background.jpg)' }}
+      style={{ backgroundImage: 'url(./images/Background.jpg)' }}
     >
       {children}
     </div>
